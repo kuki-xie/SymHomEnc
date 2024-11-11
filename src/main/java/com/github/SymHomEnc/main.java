80bceed54818e1e0c10d7df834752fe7f7babede
kuki <xieronghaii@outlook.com>
2024-11-11 14:49:01 +0800

    Update main.java

--- src/main/java/com/github/SymHomEnc/main.java
+++ src/main/java/com/github/SymHomEnc/main.java
@@ -10,15 +10,37 @@ public class main {
         SHEPublicKey pk = param.getSHEPublicKey();
         SHEPublicParameter pb = param.getSHEPublicParameter();
 
+
+        // 测量加密时间
+        long encStartTime = System.nanoTime();
+
+        BigInteger message = BigInteger.valueOf(2).pow(29);
         // Encryption with private key
-        SHECipher a = SymHomEnc.enc(123456, sk);
-        // Encryption with public key
-        SHECipher b = SymHomEnc.enc(123456, pk);
-        // Decryption with private key
-        BigInteger c = SymHomEnc.dec(a, sk);
+        SHECipher a = SymHomEnc.enc(message, sk);
+
+        long encEndTime = System.nanoTime();
+        long encDuration = encEndTime - encStartTime;
+
+        System.out.println("加密时间(nanoseconds): " + encDuration);
 
         System.out.println("密文a:" + a);
 
+
+        // Encryption with public key
+        SHECipher b = SymHomEnc.enc(123456789, pk);
+
+        // 测量加密时间
+        long decStartTime = System.nanoTime();
+
+        // Decryption with private key
+        BigInteger c = SymHomEnc.dec(a, sk);
+
+
         System.out.println("解密后的明文c:" + c);
+
+        long decEndTime = System.nanoTime();
+        long decDuration = encEndTime - encStartTime;
+
+        System.out.println("解密时间(nanoseconds): " + encDuration);
     }
 }
